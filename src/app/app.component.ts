ad0363aa6f356acab68062da165bf08c45e2a0ab
Hamza Moudani <hamza.moudani1@ibm.com>
2019-07-01 17:06:37 +0100

    round 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular7-training';
+  serverElements = [];
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type : 'server',
+      name : serverData.serverName,
+      content : serverData.serverContent
+    });
+  }
+
+  onBluePrintAdded(bluePrintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type : 'blueprint',
+      name : bluePrintData.serverName,
+      content : bluePrintData.serverContent
+    });
+  }
 }
